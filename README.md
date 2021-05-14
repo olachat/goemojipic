156e5573cc2bd17317e1e2f3e69969fe45f6dd9b
问天 Weng Wei <weng.wei@olaola.chat>
2021-05-14 17:26:11 +0800

    Add scripts

---
+++ README.md
@@ -0,0 +1,13 @@
+# goemojipic
+
+
+
+# memo
+
+Apple & Google emoji images are extracted by `go run script/main.go`
+
+# Reference
+
+* http://www.unicode.org/emoji/charts/full-emoji-list.html
+* https://segmentfault.com/a/1190000022100299
+* https://github.com/golang/freetype/issues/45
---
+++ script/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	f, _ := os.Open("full-emoji-list.html")
+	defer f.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(f)
+
+	doc.Find("td.code").Each(func(i int, s *goquery.Selection) {
+		code := strings.ReplaceAll(s.Find("a").Text(), " ", "")
+
+		img, _ := s.Next().Next().Find("img").Attr("src")
+		if len(img) < 100 {
+			fmt.Printf("Apple emoji not found: %s %s\n", code, s.Next().Text())
+		} else {
+			data, _ := base64.StdEncoding.DecodeString(img[22:])
+			ioutil.WriteFile("apple/"+code+".png", data, 0644)
+		}
+
+		img, _ = s.Next().Next().Next().Find("img").Attr("src")
+		if len(img) < 100 {
+			fmt.Printf("Google emoji not found: %s %s\n", code, s.Next().Text())
+		} else {
+			data, _ := base64.StdEncoding.DecodeString(img[22:])
+			ioutil.WriteFile("google/"+code+".png", data, 0644)
+		}
+	})
+}
